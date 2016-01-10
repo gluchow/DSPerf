4a16a3b65145b1e184ce877cb708159393f41c6d
Paul Gluchow <paul.gluchow@iteratec.de>
2016-01-10 15:35:00 +0100

    Einfacher Unit Test für die Auswertung ohne Randfälle...

---
+++ src/test/java/de/pgl/collection/measure/evaluation/MeasurementsHolderTest.java
@@ -0,0 +1,68 @@
+package de.pgl.collection.measure.evaluation;
+
+import de.pgl.collection.measure.measurement.Measurement;
+import de.pgl.collection.measure.measurement.list.IntegerArrayListCreator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MeasurementsHolderTest {
+
+    private MeasurementsHolder holder;
+
+    @Before
+    public void setUp() throws Exception {
+        holder = new MeasurementsHolder();
+    }
+
+    @Test
+    public void canAddOneMeasurement() {
+        int size = 10;
+        List<Integer> list = new IntegerArrayListCreator().createOrderedList(size);
+        holder.addMeasure(new Measurement("ArrayList", "find", size, () -> list.indexOf(0)));
+        Assert.assertEquals("Should have one measurement.", 1, holder.countOfAllMeasurements());
+    }
+
+    @Test
+    public void shouldCalculateCorrectMin() {
+        String configName = "test_x";
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 12L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 30L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 10L));
+        long actualMin = holder.getMinDurations().get(configName + "-min");
+        Assert.assertEquals(10L, actualMin);
+    }
+
+    @Test
+    public void shouldCalculateCorrectAvg() {
+        String configName = "test_x";
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 12L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 30L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 10L));
+        long actualAvg = holder.getAvgDurations().get(configName + "-avg");
+        Assert.assertEquals(17L, actualAvg);
+    }
+
+    @Test
+    public void shouldCalculateCorrectMax() {
+        String configName = "test_x";
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 12L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 30L));
+        holder.addMeasure(createMeasurementMockWithDuration(configName, 10L));
+        long actualMax = holder.getMaxDurations().get(configName + "-max");
+        Assert.assertEquals(30L, actualMax);
+    }
+
+    private Measurement createMeasurementMockWithDuration(String configName, Long duration) {
+        Measurement measurement = mock(Measurement.class);
+        when(measurement.getConfig()).thenReturn(configName);
+        when(measurement.getDurationInMs()).thenReturn(duration);
+        return measurement;
+    }
+
+}
